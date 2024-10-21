4d59f147a4f04b0bb69ebf5cfa2820334b9eb399
uthso297 <uthso119464@gmail.com>
2024-10-22 00:09:49 +0600

    completed navbar and proptypes

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks/Bookmarks'
+import Header from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+
+      <Header></Header>
+      
+      <div className='md:flex'>
+      <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
